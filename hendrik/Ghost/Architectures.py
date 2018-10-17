6e917d87aa7fc80aeb29655326b75090df54a7d8
Hendrik Vloet <hendrik.vloet@gmx.de>
2018-10-17 22:48:32 +0200

    updated pytorch tutorial dqn architecture

--- hendrik/Ghost/Architectures.py
+++ hendrik/Ghost/Architectures.py
@@ -23,8 +23,7 @@ import numpy as np
 #
 #  This Class contains the DQN network architecture according to
 #  https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
-#  but has slightly modified layer structure
-class PytorchTutorialDQN(nn.Module):
+#  but has slightly modified layer structureclass PytorchTutorialDQN(nn.Module):
   ## Constructor
   def __init__(self, FLAGS):
     super(PytorchTutorialDQN, self).__init__()
@@ -34,19 +33,16 @@ class PytorchTutorialDQN(nn.Module):
     self.xy_space = np.transpose([np.tile(x_space, len(y_space)), np.repeat(y_space, len(x_space))])
     self.conv1 = nn.Conv2d(1, 16, kernel_size=5, stride=2)
     self.bn1 = nn.BatchNorm2d(16)
-    self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=2)
+    self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
     self.bn2 = nn.BatchNorm2d(32)
-    self.conv3 = nn.Conv2d(32, 32, kernel_size=3, stride=2)
+    self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
     self.bn3 = nn.BatchNorm2d(32)
-    self.conv4 = nn.Conv2d(32, 64, kernel_size=3, stride=2)
-    self.bn4 = nn.BatchNorm2d(64)
 
     self.tmp_w = self._get_filter_dimension(84, 5 , 0 , 2)
-    self.tmp_w = self._get_filter_dimension(self.tmp_w, 3 , 0 , 2)
-    self.tmp_w = self._get_filter_dimension(self.tmp_w, 3 , 0 , 2)
-    self.tmp_w = self._get_filter_dimension(self.tmp_w, 3 , 0 , 2)
+    self.tmp_w = self._get_filter_dimension(self.tmp_w, 5 , 0 , 2)
+    self.tmp_w = self._get_filter_dimension(self.tmp_w, 5 , 0 , 2)
 
-    self.fc1 = nn.Linear(64 * self.tmp_w * self.tmp_w, 512)
+    self.fc1 = nn.Linear(32 * self.tmp_w * self.tmp_w, 512)
 
     self.head_actions = nn.Linear(512, len(self.xy_space))
 
@@ -64,6 +60,13 @@ class PytorchTutorialDQN(nn.Module):
   def _get_filter_dimension(self, w, f, p, s):
     return int((w - f + 2*p) / s + 1)
 
+  def _num_flat_features(self, x):
+    size = x.size()[1:]
+    num_features = 1
+    for s in size:
+      num_features *= s
+    return num_features
+
   ## Forward pass
   #
   #  passes the state information through the network and approximate
@@ -73,17 +76,11 @@ class PytorchTutorialDQN(nn.Module):
     screen = F.relu(self.bn1(self.conv1(screen)))
     screen = F.relu(self.bn2(self.conv2(screen)))
     screen = F.relu(self.bn3(self.conv3(screen)))
-    screen = F.relu(self.bn4(self.conv4(screen)))
-    screen = screen.view(-1, 64 * self.tmp_w * self.tmp_w)
+    screen = screen.view(-1, self._num_flat_features(screen))
     screen = F.relu(self.fc1(screen))
-
     action_q = self.head_actions(screen)
 
     return action_q
-
-
-
-
 
 ## ConvNet
 class ConvNet(nn.Module):
